6ddcb778812864e2ffc90cf0c0a180022277dbae
mariemcdd <mariemcdermott221@riversideunified.org>
2024-02-21 12:05:08 -0800

    start animation

--- 3D Platformer Project/Assets/Scripts/PlayerAnimation.cs
+++ 3D Platformer Project/Assets/Scripts/PlayerAnimation.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class PlayerAnimation : MonoBehaviour
 {
-public float rotationSpeed = 50f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +13,6 @@ public float rotationSpeed = 50f;
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        
     }
 }
